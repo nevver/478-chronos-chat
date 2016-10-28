77db8241bc027e2497e451815d168454549dad21
bryan <io.nevver@yahoo.com>
2016-10-27 18:46:48 -0700

    changes JWT expiration to 5 minutes

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,5 +1,6 @@
 class JsonWebToken
   def self.encode(payload)
+  	payload[:exp] = (5).minutes.from_now.to_i #expire in 5 minutes
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
