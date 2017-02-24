0dd6be3d6bf359ac1902f7eb102c79c374828f44
nevver <io.nevver@yahoo.com>
2017-02-24 00:07:09 +0000

    Update repo

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  #devise_for :users
+  get '' => 'home#home'
+  get  'home' => 'home#index'
+  post 'authenticate' => 'authentication#authenticate_user'
+  post 'registration' => 'authentication#register_user'
+  get  '/conversations' => 'conversations#home'
+  get  '/conversations/create' => 'conversations#home'
+  get  '/conversations/index' => 'conversations#index'
+  post '/conversations/create' => 'conversations#create'
+  get  '/messages' => 'messages#home'
+  get  '/messages/create' => 'messages#home'
+  get  '/messages/index' => 'messages#index'
+  post '/messages/create' => 'messages#create'
+end
